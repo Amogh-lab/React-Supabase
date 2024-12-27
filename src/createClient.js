95b0cb041d95ef9cfd422795eadd44277254a2b8
Amogh-lab <amoghap099@gmail.com>
2024-12-27 21:11:00 +0530

    Client js

--- src/createClient.js
+++ src/createClient.js
@@ -5,10 +5,3 @@ export const supabase = createClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4bm13bnRucm5ieGJqd3hsbnlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUwMzQ0MjIsImV4cCI6MjA1MDYxMDQyMn0.3esOWR5vOEKbzY-0eUUmXnlc2u60pDjhvW4yOJvKXrM"
 )
 
-// import { createClient } from "@supabase/supabase-js";
-
-
-// export const supabase= createClient(
-//     "https://tupmvvksqgbqdzridrqb.supabase.co",
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1cG12dmtzcWdicWR6cmlkcnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjcwMTIyNTcsImV4cCI6MTk4MjU4ODI1N30.5uZdCofPa878ZbK6jbrl6pJBLmn11XC9BAausn2tD2U"
-//     )
